62652977cfa8b746b83a0d373d52fe718b93ac3c
jerrythunt <jhunt52@my.bcit.ca>
2025-04-30 11:48:12 -0700

    Addded comments for context to code

--- testing/app.py
+++ testing/app.py
@@ -1,3 +1,5 @@
+"""THIS MODULE LAUNCHES THE WEB SERVER"""
+
 from flask import Flask, render_template, redirect, url_for, jsonify, request
 from sqlalchemy.exc import OperationalError
 from pathlib import Path
@@ -6,12 +8,18 @@ from models import Time, Seed
 from datetime import datetime as dt
 import time
 
+"""INITIATES  APPLICATION, DO NOT TOUCH"""
 app = Flask(__name__)
 
+# name of database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Growing_Plants.db"
+# populates database in current folder
 app.instance_path = Path(".").resolve()
 
 db.init_app(app)
+"""DO NOT TOUCH ABOVE HERE ^"""
+
+"""BEGINNING OF LOGIC STARTS HERE"""
 
 # A fixture to view all seeds in db
 def call_seeds():
@@ -23,8 +31,11 @@ def call_seeds():
 
 # Calls current time from db 
 def call_time_update():
+    # Query the database for a Time table, and grab the specific entry where the id == 1
     statement = db.select(Time).where(Time.id == 1)
+    # Execute the statement
     records = db.session.execute(statement)
+    # Convert the statement/query to an object so we can use it (.scalar()/.scalars()) 
     results = records.scalar()
  
     # If it doesn't exist yet (app has been initialized), store current time to database
@@ -39,14 +50,18 @@ def call_time_update():
 
     return results
 
+# Update the current time entry in database to right now
 def update_time():
+    # Query the database for a table named Time, and grab the first row
     current_time_row = db.session.query(Time).first()
+    # Update the first row to current time 
     current_time_row.current_time = dt.now()
+    # Commit the change to the database
     db.session.commit()
 
     return current_time_row
 
-# Tell user when they first started playing
+# Tell user when they first started playing the game
 def began_game():  
     statement = db.select(Time).where(Time.id == 2)
     records = db.session.execute(statement)
@@ -64,7 +79,9 @@ def began_game():
 
     return results
 
+# Home page of app
 @app.route("/")
+# When home page is loaded, run these functions
 def home():
     seeds = call_seeds()
     # 'time_update' MUST run before 'start' (check pk's)
@@ -72,8 +89,10 @@ def home():
     start = began_game()
     update_time()
 
+    # use the "home.html" template, store the result of each function to a variable that can be called in the html file. Ex: {{plant.name}}
     return render_template("home.html", plant = seeds, time_update = time_update, start = start)
 
+# Allows user to plant an unplanted seed
 @app.route("/plant/<int:seed_id>", methods=["POST"])
 def plant_seed(seed_id):
     seed = db.get_or_404(Seed, seed_id)
@@ -81,6 +100,7 @@ def plant_seed(seed_id):
     
     return redirect(url_for("home"))
 
+# Allows user to water a seed if not watered
 @app.route("/water/<int:seed_id>", methods=["POST"])
 def water_seed(seed_id):
     seed = db.get_or_404(Seed, seed_id)
@@ -88,13 +108,6 @@ def water_seed(seed_id):
     
     return redirect(url_for("home"))
 
-
-
-# For every planted seed in database
-## check time_diff and apply business rules/logic that would've happened had it been running
-## list their watered state - Completed
-## list their growth stage  - Incomplete
-## What else?
-
+# When app.py is run, it runs in debug mode on localhost:8888 
 if __name__ == "__main__":
     app.run(debug = True, port = 8888)
--- testing/models.py
+++ testing/models.py
@@ -2,9 +2,17 @@ from db import db
 from datetime import datetime as dt
 import time
 
+# Classes are blueprints to create a real-world object
+# Objects have attributes that define what they ARE (variable)
+# Objects have methods that define what they DO (functions) 
+
+# Create a table, inherit 'db.Model', otherwise it wont add to database!!
 class Time(db.Model):
+    # Declare the tablename
     __tablename__ = "Time"
 
+    # Declare attributes that will populate database 
+    # attribute = (db.mapped_column(db.DataType, options = insert_option))
     id = db.mapped_column(db.Integer, primary_key = True)
     name = db.mapped_column(db.String, nullable = False)
     current_time = db.mapped_column(db.DateTime, nullable = False, default = dt.now())
@@ -22,10 +30,21 @@ class Seed(db.Model):
     is_planted = db.mapped_column(db.Boolean, nullable=False, default=False)    
     time_of_planting = db.mapped_column(db.DateTime, nullable = True, default = None)
 
+    # Declare methods that we can call to update attributes
+    # def function(self):
+    # 'self' allows the object instance to call itself, so you don't have to hardcode
     def plant(self):
+        # If seed object is planted, dont allow this method
+        if self.is_planted == True:
+            print(f'{self.name} has been planted already!')
+            return f'{self.name} has been planted already!'
+        # Else, run this logic 
         self.is_planted = True
+        # dt.now() grabs the current time and stores it as an object
         self.time_of_planting = dt.now()
+        # Update the database
         db.session.commit()
+        # Debug check
         print(f'A user planted a {self.name} on {self.time_of_planting}')
         return self.is_planted
     
@@ -47,6 +66,7 @@ class Seed(db.Model):
         print(f"{self.name} has been watered. It looks very happy!")
         return f'A user watered a {self.name} at {str(self.time_of_watering)}!'
     
+    # Reset is_watered status
     def reset_is_watered(self):
         self.is_watered = False
         db.session.commit()
