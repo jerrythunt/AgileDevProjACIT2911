b3b9c0a66d49e1d200618cb81abe3a56c5c518aa
jerrythunt <jerrythunt@outlook.com>
2025-04-30 01:21:21 -0700

    Web server is running. User can plant and water seeds that update database tables while receiving visual confirmation

--- testing/models.py
+++ testing/models.py
@@ -6,7 +6,8 @@ class Time(db.Model):
     __tablename__ = "Time"
 
     id = db.mapped_column(db.Integer, primary_key = True)
-    current_time = db.mapped_column(db.DateTime, nullable = False, default = None)
+    name = db.mapped_column(db.String, nullable = False)
+    current_time = db.mapped_column(db.DateTime, nullable = False, default = dt.now())
 
 class Seed(db.Model):
     __tablename__ = "seeds"
@@ -25,12 +26,16 @@ class Seed(db.Model):
         self.is_planted = True
         self.time_of_planting = dt.now()
         db.session.commit()
-        return f'A user planted a {self.name} on {self.time_of_planting}'
+        print(f'A user planted a {self.name} on {self.time_of_planting}')
+        return self.is_planted
     
     def water_plant(self):
+        if self.is_planted == False:
+            print(f"{self.name} hasn't been planted yet. How could you water it if it isn't planted?")
+            return f"{self.name} hasn't been planted yet. How could you water it if it isn't planted?"
         if self.is_watered == True:
-            return f"{self.name} is already watered!"
-            pass
+            print(f"{self.name} is already watered! You have to wait 5 minutes before watering again")
+            return f"{self.name} is already watered! You have to wait 5 minutes before watering again"
         
         self.is_watered = True
         self.growth = self.growth + 20
@@ -39,6 +44,7 @@ class Seed(db.Model):
             self.hp = 100
         self.hp = self.hp + 20
         db.session.commit()
+        print(f"{self.name} has been watered. It looks very happy!")
         return f'A user watered a {self.name} at {str(self.time_of_watering)}!'
     
     def reset_is_watered(self):
